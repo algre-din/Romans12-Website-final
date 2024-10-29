dc731463d9699fb9b5f2d16c269dac74bc085750
algre-din <algre_din@asdah.school.nz>
2024-10-29 16:06:25 +1300

    added Javascript file for tooltip, minor tidy-ups in html

---
+++ tooltip.js
@@ -0,0 +1,89 @@
+// This is the codes for tooltip where you hover over a word and a message pops up 
+
+ 
+//Verses 1-2//
+document.addEventListener("DOMContentLoaded", function() { 
+
+    var v1v2 = document.querySelector('.v1v2text');
+    v1v2.textContent = "1. I beseech you therefore, brethren, by the mercies of God, that ye present your bodies a living sacrifice, holy, acceptable unto God, which is your reasonable service. 2. And be not conformed to this world: but be ye transformed by the renewing of your mind, that ye may prove what is that good, and acceptable, and perfect, will of God.";
+
+    var v1v2i = document.querySelector('.v1v2itext');
+    v1v2i.textContent = "1. I beseech you therefore, brethren, by the mercies of God, that ye present your bodies a living sacrifice, holy, acceptable unto God, which is your reasonable service. 2. And be not conformed to this world: but be ye transformed by the renewing of your mind, that ye may prove what is that good, and acceptable, and perfect, will of God.";
+
+    var v1v2ii = document.querySelector('.v1v2iitext');
+    v1v2ii.textContent = "1. I beseech you therefore, brethren, by the mercies of God, that ye present your bodies a living sacrifice, holy, acceptable unto God, which is your reasonable service. 2. And be not conformed to this world: but be ye transformed by the renewing of your mind, that ye may prove what is that good, and acceptable, and perfect, will of God.";
+
+    var v1 = document.querySelector('.v1text');
+    v1.textContent = "1. I beseech you therefore, brethren, by the mercies of God, that ye present your bodies a living sacrifice, holy, acceptable unto God, which is your reasonable service.";
+
+    var v2 = document.querySelector('.v2text');
+    v2.textContent = "2. And be not conformed to this world: but be ye transformed by the renewing of your mind, that ye may prove what is that good, and acceptable, and perfect, will of God.";
+
+    var v2i = document.querySelector('.v2itext');
+    v2i.textContent = "2. And be not conformed to this world: but be ye transformed by the renewing of your mind, that ye may prove what is that good, and acceptable, and perfect, will of God.";
+
+    
+}); 
+
+//Verses3-8//
+document.addEventListener("DOMContentLoaded", function() { 
+    var v3v8 = document.querySelector('.v3v8text');
+    v3v8.textContent = "3. For I say, through the grace given unto me, to every man that is among you, not to think of himself more highly than he ought to think; but to think soberly, according as God hath dealt to every man the measure of faith. 4. For as we have many members in one body, and all members have not the same office: 5. So we, being many, are one body in Christ, and every one members one of another. 6. Having then gifts differing according to the grace that is given to us, whether prophecy, let us prophesy according to the proportion of faith; 7. Or ministry, let us wait on our ministering: or he that teacheth, on teaching; 8. Or he that exhorteth, on exhortation: he that giveth, let him do it with simplicity; he that ruleth, with diligence; he that sheweth mercy, with cheerfulness.";
+
+    var v3v8i = document.querySelector('.v3v8itext');
+    v3v8i.textContent = "3. For I say, through the grace given unto me, to every man that is among you, not to think of himself more highly than he ought to think; but to think soberly, according as God hath dealt to every man the measure of faith. 4. For as we have many members in one body, and all members have not the same office: 5. So we, being many, are one body in Christ, and every one members one of another. 6. Having then gifts differing according to the grace that is given to us, whether prophecy, let us prophesy according to the proportion of faith; 7. Or ministry, let us wait on our ministering: or he that teacheth, on teaching; 8. Or he that exhorteth, on exhortation: he that giveth, let him do it with simplicity; he that ruleth, with diligence; he that sheweth mercy, with cheerfulness.";
+
+    var v3 = document.querySelector('.v3text');
+    v3.textContent = "3. For I say, through the grace given unto me, to every man that is among you, not to think of himself more highly than he ought to think; but to think soberly, according as God hath dealt to every man the measure of faith.";
+
+    var v4v5 = document.querySelector('.v4v5text');
+    v4v5.textContent = "4. For as we have many members in one body, and all members have not the same office: 5. So we, being many, are one body in Christ, and every one members one of another.";
+
+    var v4v5i = document.querySelector('.v4v5itext');
+    v4v5i.textContent = "4. For as we have many members in one body, and all members have not the same office: 5. So we, being many, are one body in Christ, and every one members one of another.";
+
+    var v6v8 = document.querySelector('.v6v8text');
+    v6v8.textContent = "6. Having then gifts differing according to the grace that is given to us, whether prophecy, let us prophesy according to the proportion of faith; 7. Or ministry, let us wait on our ministering: or he that teacheth, on teaching; 8. Or he that exhorteth, on exhortation: he that giveth, let him do it with simplicity; he that ruleth, with diligence; he that sheweth mercy, with cheerfulness.";
+
+
+});
+
+//Verses 9-16//
+document.addEventListener("DOMContentLoaded", function() { 
+    var v9v16 = document.querySelector('.v9v16text');
+    v9v16.textContent = "9 Let love be without dissimulation. Abhor that which is evil; cleave to that which is good. 10. Be kindly affectioned one to another with brotherly love; in honour preferring one another; 11. Not slothful in business; fervent in spirit; serving the Lord; 12. Rejoicing in hope; patient in tribulation; continuing instant in prayer; 13. Distributing to the necessity of saints; given to hospitality. 14. Bless them which persecute you: bless, and curse not. 15. Rejoice with them that do rejoice, and weep with them that weep. 16. Be of the same mind one toward another. Mind not high things, but condescend to men of low estate. Be not wise in your own conceits.";
+
+    var v9 = document.querySelector('.v9text');
+    v9.textContent = "9. Let love be without dissimulation. Abhor that which is evil; cleave to that which is good.";
+
+    var v11 = document.querySelector('.v11text');
+    v11.textContent = "11. Not slothful in business; fervent in spirit; serving the Lord;";
+
+    var v15 = document.querySelector('.v15text');
+    v15.textContent = "15. Rejoice with them that do rejoice, and weep with them that weep.";
+
+    var v16 = document.querySelector('.v16text');
+    v16.textContent = "16. Be of the same mind one toward another. Mind not high things, but condescend to men of low estate. Be not wise in your own conceits.";
+
+});
+
+//Verses 14, 17-20//
+document.addEventListener("DOMContentLoaded", function() { 
+    var v14 = document.querySelector('.v14text');
+    v14.textContent = "14. Bless them which persecute you: bless, and curse not.";
+
+    var v17v18 = document.querySelector('.v17v18text');
+    v17v18.textContent = "17. Recompense to no man evil for evil. Provide things honest in the sight of all men. 18. If it be possible, as much as lieth in you, live peaceably with all men.";
+
+    var v19 = document.querySelector('.v19text');
+    v19.textContent = "19. Dearly beloved, avenge not yourselves, but rather give place unto wrath: for it is written, Vengeance is mine; I will repay, saith the Lord.";
+
+    var v20 = document.querySelector('.v20text');
+    v20.textContent = "20. Therefore if thine enemy hunger, feed him; if he thirst, give him drink: for in so doing thou shalt heap coals of fire on his head.";
+});
+
+//Verses 21//
+document.addEventListener("DOMContentLoaded", function() { 
+    var v21 = document.querySelector('.v21text');
+    v21.textContent = "21 Be not overcome of evil, but overcome evil with good.";
+});
